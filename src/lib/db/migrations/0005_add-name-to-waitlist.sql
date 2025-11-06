4cfb2b40a04e9fe784085ce4219ffce8aab25b62
Daniel Ikoyo <dannirolands@gmail.com>
2025-11-06 23:44:17 +0100

    refactor: waitlist db migration

--- src/lib/db/migrations/0005_add-name-to-waitlist.sql
+++ src/lib/db/migrations/0005_add-name-to-waitlist.sql
@@ -2,15 +2,10 @@
 ALTER TABLE "waitlist" ADD COLUMN "name" text;
 
 -- Populate name column for existing waitlist entries from users table
-UPDATE "waitlist" w
-SET "name" = u."name"
-FROM "users" u
-WHERE LOWER(w."email") = LOWER(u."email");
+UPDATE "waitlist" w SET "name" = u."name" FROM "users" u WHERE LOWER(w."email") = LOWER(u."email");
 
 -- Set default name for any remaining entries without a match
-UPDATE "waitlist"
-SET "name" = 'Unknown User'
-WHERE "name" IS NULL;
+UPDATE "waitlist" SET "name" = 'Unknown User' WHERE "name" IS NULL;
 
 -- Now make the column NOT NULL
 ALTER TABLE "waitlist" ALTER COLUMN "name" SET NOT NULL;
